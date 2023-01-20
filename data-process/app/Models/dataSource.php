08a7896ed884870c8f6ee0c0c9c2db797363a890
Chamidu-circlebook <72206025+ChamiduDev-5224@users.noreply.github.com>
2023-01-20 19:21:15 +0530

    dataSource model created

---
+++ data-process/app/Models/dataSource.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class dataSource extends Model
+
+{    protected $table = 'data_source_x';
+     use HasFactory;
+}
